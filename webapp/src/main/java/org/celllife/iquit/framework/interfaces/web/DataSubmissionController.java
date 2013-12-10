8ef9b18831d2920530c6681ad498f1a68d770619
Dine <dine@cell-life.org>
2013-12-10 16:56:59 +0200

    INTERNAL-392 Smoking Cessation - Webapp

--- webapp/src/main/java/org/celllife/iquit/framework/interfaces/web/DataSubmissionController.java
+++ webapp/src/main/java/org/celllife/iquit/framework/interfaces/web/DataSubmissionController.java
@@ -1,7 +1,8 @@
 package org.celllife.iquit.framework.interfaces.web;
 
 import org.apache.commons.lang.StringEscapeUtils;
-import org.springframework.security.crypto.codec.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
@@ -19,9 +18,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.codec.binary.Base64;
 
 @Controller
 public class DataSubmissionController {
+
+    private static Logger log = LoggerFactory.getLogger(DataSubmissionController.class);
 
     @ResponseBody
     @RequestMapping(value = "/service/iquit-form", method = RequestMethod.POST)
@@ -44,31 +46,24 @@ public class DataSubmissionController {
     private void sendDataToCapture(String data) throws Exception {
 
         URL url = new URL("http://localhost:8080/capture/rest/studies/1/forms/2/versions/2/data");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/xml");
-        byte[] encoded = Base64.encode("admin:poekie1".getBytes());
-        con.setRequestProperty("Authorization", "Basic " + encoded);
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Content-Type", "application/xml");
+        String encodedString = new String(Base64.encodeBase64("admin:poekie1".getBytes("UTF-8")), "UTF-8");
+        httpURLConnection.setRequestProperty("Authorization", "Basic " + encodedString);
+        httpURLConnection.setDoOutput(true);
 
-        // Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(data);
-        wr.flush();
-        wr.close();
+        DataOutputStream dataOutputStream = new DataOutputStream(httpURLConnection.getOutputStream());
+        dataOutputStream.writeBytes(data);
+        dataOutputStream.flush();
+        dataOutputStream.close();
 
-        int responseCode = con.getResponseCode();
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        int responseCode = httpURLConnection.getResponseCode();
+
+        if (responseCode != 200) {
+            log.error("Could not submit data to the Capture server.");
         }
-        in.close();
-        //print result
-        System.out.println(response.toString());
-
     }
 
 
@@ -77,11 +72,11 @@ public class DataSubmissionController {
         String xmlString = "<FormData><data>" + StringEscapeUtils.escapeHtml("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 
         xmlString = xmlString.concat(StringEscapeUtils.escapeHtml("<example_study_iquit_form_v1 " +
-                "xmlns=\"http://www.w3.org/2002/xforms\" " +
+                "xmlns=\"http://www.w3.org/2002/xforms\" "+
+                "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
                 "name=\"iQuit Form_v1\" " +
                 "id=\"2\" " +
-                "formKey=\"example_study_iquit_form_v1\" " +
-                "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"));
+                "formKey=\"example_study_iquit_form_v1\">"));
 
         for (String parameter : parameterMap.keySet()) {
 
@@ -90,13 +85,13 @@ public class DataSubmissionController {
                 xmlString = xmlString.concat(StringEscapeUtils.escapeHtml("<" + parameter + " " +
                         "xmlns=\"http://www.w3.org/2002/xforms\">" +
                         parameterMap.get(parameter).get(0) +
-                        "<\\" + parameter + ">"));
+                        "</" + parameter + ">"));
             }
 
         }
 
-        xmlString.concat(StringEscapeUtils.escapeHtml("/example_study_iquit_form_v1>\n") +
-                "</FormData></data>");
+        xmlString = xmlString.concat(StringEscapeUtils.escapeHtml("</example_study_iquit_form_v1>") +
+                "</data></FormData>");
 
         return xmlString;
 
